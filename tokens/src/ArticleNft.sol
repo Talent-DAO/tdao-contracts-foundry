3ae2705c568810da2ae63bb874b624427913434d
Jaxcoder <codenamejason@outlook.com>
2022-11-01 10:26:28 -0400

    tests pass setup

--- tokens/src/ArticleNft.sol
+++ tokens/src/ArticleNft.sol
@@ -23,78 +23,75 @@ contract ArticleNft is Ownable, ERC721URIStorage, AuthorEntity {
     address public tDaoTokenAddress;
     IERC20 private tDaoToken;
 
-    constructor(address _owner, address _tDaoToken) public ERC721("Talent DAO IP NFT", "TDAO-IPNFT") {
+    constructor(address _owner, address _tDaoToken)
+        ERC721("Talent DAO IP NFT", "TDAO-IPNFT")
+    {
         tDaoToken = IERC20(_tDaoToken);
         tDaoTokenAddress = _tDaoToken;
         _transferOwnership(_owner);
     }
 
     /// @dev for OpenSea
-    function contractURI() public pure returns (string memory)
-    {
+    // todo: add metadata
+    function contractURI() public pure returns (string memory) {
         return "";
     }
 
     /// @dev this is internal mint function
     /// @param ownerAddress the user that is minting the token address
     /// @param amount the amount of tdao tokens submitting
-    function mintNFTForArticle(address ownerAddress, string memory tokenURI, uint256 amount)
-        public
-        returns (uint256)
-    {
-        //require(tDaoToken.balanceOf(msg.sender) > amount, "You don't have enough TDAO tokens");
-        // todo: How much TALENT token do we ask for?
-        // take their money
-        //tDaoToken.transferFrom(ownerAddress, treasury, amount);
-
+    function mintNFTForArticle(
+        address ownerAddress,
+        string memory arweaveHash,
+        string memory metadataPtr,
+        uint256 amount
+    ) public returns (uint256) {
         _tokenIds.increment();
         uint256 authorId;
 
-        // (uint256 articleId) = addArticle(authorAddress, arweaveHash, metadataPtr, amount);
-        // Article storage article = articles[arweaveHash];
-        // article.author = authorAddress;
-        // article.metadataPtr = metadataPtr;
-        // article.paid = amount;
+        uint256 articleId = addArticle(
+            ownerAddress,
+            arweaveHash,
+            metadataPtr,
+            amount
+        );
+        Article storage article = articles[arweaveHash];
+        article.author = ownerAddress;
+        article.metadataPtr = metadataPtr;
+        article.paid = amount;
 
-        // // check if author exists first
-        // Author storage author = authors[authorAddress];
-        // if(author.authorAddress != authorAddress){
-        //     (authorId) = addAuthor(authorAddress, articleId, profileHash);
-        // }
-        // authorId = author.id;
-        // // add the article to the author
-       
-        // author.articles[articleId] = article;
+        // check if author exists first
+        Author storage author = authors[ownerAddress];
+        if (author.authorAddress != ownerAddress) {
+            (authorId) = addAuthor(
+                ownerAddress,
+                articleId,
+                /*profileHash*/
+                "profileHash"
+            );
+        }
+        authorId = author.id;
+        // add the article to the author
+
+        author.articles.push(article);
 
         // mint the nft to the author/owner
         uint256 newItemId = _tokenIds.current();
         _mint(ownerAddress, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, arweaveHash);
 
         // return the tokenId and the authorId it was minted to
-        // the authorId will be 
+        // the authorId will be
         return (newItemId);
     }
 
     /// @dev public function to set the token URI
     /// @param _tokenId the id of the token to update
     /// @param _tokenURI the new token URI
-    function setTokenURI
-    (
-        uint256 _tokenId,
-        string memory _tokenURI
-    )
+    function setTokenURI(uint256 _tokenId, string memory _tokenURI)
         public
         onlyOwner
     {
         _setTokenURI(_tokenId, _tokenURI);
-    }
-
-    function getContractBalance(address token) public view onlyOwner returns (uint256) {
-        return IERC20(token).balanceOf(address(this));
-    }
-
-    function withdrawTDAOTokens() public onlyOwner {
-        tDaoToken.transferFrom(address(this), _msgSender(), tDaoToken.balanceOf(address(this)));
     }
 }
